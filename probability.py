fee8e14eca7807cc4e7cf2419e64d4c22dbe075e
milevskyi <dmytro.milevskyi@geocomply.com>
2022-07-01 22:54:53 +0300

    Checking type annotations with mypy

--- probability.py
+++ probability.py
@@ -1,22 +1,27 @@
 # Source: http://rosettacode.org/wiki/Dice_game_probabilities#Python
 from itertools import product
+from typing import List, Tuple, Any
 
 
-def gen_dict(n_faces, n_dice):
+def gen_dict(n_faces: int, n_dice: int) -> Tuple[List[int], Any]:
     counts = [0] * ((n_faces + 1) * n_dice)
     for t in product(range(1, n_faces + 1), repeat=n_dice):
         counts[sum(t)] += 1
-    return counts, n_faces ** n_dice
+    return counts, n_faces**n_dice
 
 
-def beating_probability(n_sides1, n_dice1, n_sides2, n_dice2):
+def beating_probability(
+    n_sides1: int, n_dice1: int, n_sides2: int, n_dice2: int
+) -> float:
     c1, p1 = gen_dict(n_sides1, n_dice1)
     c2, p2 = gen_dict(n_sides2, n_dice2)
     p12 = float(p1 * p2)
 
-    return sum(p[1] * q[1] / p12
-               for p, q in product(enumerate(c1), enumerate(c2))
-               if p[0] > q[0])
+    return sum(
+        p[1] * q[1] / p12
+        for p, q in product(enumerate(c1), enumerate(c2))
+        if p[0] > q[0]
+    )
 
 
 print(beating_probability(4, 9, 6, 6))
